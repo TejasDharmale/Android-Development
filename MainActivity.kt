7724bbe337789e2209e815d1772ee891a97c5ff3
Tejas Rajesh Dharmale <97274893+TejasDharmale@users.noreply.github.com>
2024-01-27 17:00:37 +0530

    shopping app

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,64 +1,44 @@
-package com.example.diceroller
+package com.example.shoppingapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.diceroller.ui.theme.DiceRollerTheme
+import com.example.shoppingapplication.ui.theme.ShoppingApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceRollerTheme {
-
-                DiceRollerApp()
-
+            ShoppingApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
                 }
             }
-
         }
-    }
-@Preview
-@Composable
-fun DiceRollerApp(){
-    DiceRollerWithButtonAndImage()
-}
 
-@Composable
-fun DiceRollerWithButtonAndImage(modifier: Modifier = Modifier){
-    var result by remember {
-        mutableStateOf(1)
-
-    }
-    val imageResource = when(result){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3-> R.drawable.dice_4
-            4 -> R.drawable.dice_5
-            5 -> R.drawable.dice_3
-            else -> R.drawable.dice_6
-        }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-
-        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result= (1..6).random()}) {
-            Text(stringResource(R.string.roll))
-        }
     }
 }
+
 
